fec26be135ca744b858d5437ab661302cd2cdbcb
Jeremias <jroldanzocco@gmail.com>
2022-06-07 16:03:28 -0300

    Add operator that takes operator and number and return basic function

--- src/script.js
+++ src/script.js
@@ -11,7 +11,18 @@ function multiply(n1,n2){
 }
 
 function divide(n1,n2){
-    return n1,n2;
+    return n1 / n2;
 }
 
+function operate(operator,n1,n2){
+    if (operator === "+"){
+        return add(n1,n2)
+    }else if(operator === "-"){
+        return substract(n1,n2);
+    }else if(operator === "*"){
+        return multiply(n1,n2);
+    }else if(operator === "/"){
+        return divide(n1,n2);
+    }
+}
 
