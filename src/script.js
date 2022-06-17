cfe32729b1c6e27ddfd2e8feae7cdfe58af0d683
Jeremias <jroldanzocco@gmail.com>
2022-06-16 22:09:56 -0300

    added logic for calculation

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+/* Basic functions */
 function add (n1,n2){
     return n1 + n2;
 }
@@ -14,6 +15,7 @@ function divide(n1,n2){
     return n1 / n2;
 }
 
+/*  Operative functions    */
 function operate(operator,n1,n2){
     if (operator === "+"){
         return add(n1,n2)
@@ -25,5 +27,84 @@ function operate(operator,n1,n2){
         return divide(n1,n2);
     }
 }
+/*   DOM functions */
+
+function allClean(){
+    document.querySelector('.current-oper').textContent = "0";
+    document.querySelector('.previous-oper').textContent = "";
+}
+
+function backspace(){
+    if(current.textContent !== "0"){
+   current.textContent = current.textContent.slice(0,-1)
+        if(current.textContent.length < 1){
+            current.textContent = "0"
+        }
+    }
+}
+
+function equal(){
+    
+    previous.textContent = current.textContent.replace(/[^-()\d/*+.]/g, '')
+    current.textContent = eval(current.textContent.replace(/[^-()\d/*+.]/g, ''))
+}
+
+
+/* Selectors */
+
+
+const $ac = document.querySelector('.all-clear')
+const $delete = document.querySelector('.delete')
+const $point = document.querySelector('.point')
+const $numbers = document.querySelectorAll('.numbers')
+
+const $equal = document.querySelector('.equal')
+
+
+/*  DISPLAY    */
+const current = document.querySelector('.current-oper')
+const previous = document.querySelector('.previous-oper')
+
+
+/*  OPERATORS   */
+
+const $operators = document.querySelectorAll('.operation')
+
+$operators.forEach(e => e.addEventListener('click', ()=>{
+    if (!current.textContent.slice(-1).includes(e.textContent)){
+    current.textContent += e.textContent
+    }
+}))
+
+/* Action Buttons */
+
+$point.addEventListener('click',()=>{
+    if (!current.textContent.includes(".")){
+        current.textContent += "."
+    }
+})
+
+$ac.addEventListener('click',allClean)
+$delete.addEventListener('click',backspace)
+$equal.addEventListener('click', equal)
+
+$numbers.forEach(e => e.addEventListener('click',() => {
+    if (current.textContent == "0"){
+        current.textContent = e.textContent
+    }else{
+        current.textContent += e.textContent
+    }
+}))
+
+
+
+
+
+
+
+
+
+
+
 
 
